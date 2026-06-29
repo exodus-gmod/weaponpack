d545ef58ef6cbe61756531498ccc770d8ee7d9d3
LanceLionhart <marek.murray06@gmail.com>
2026-06-29 14:41:10 -0700

    Groza Attachment Patch

--- weapon_pack/lua/weapons/tfa_ins2_groza.lua
+++ weapon_pack/lua/weapons/tfa_ins2_groza.lua
@@ -340,7 +340,7 @@ SWEP.WElements = {
 }
 SWEP.Attachments = {
 	[1] = { atts = { "ins2_br_supp", "r6s_muzzle_2", "r6s_flashhider_2" }, order = 2 },
-	[2] = { atts = { "am_match", "am_magnum", "am_gib" }, order = 6 },
+	[3] = { atts = { "am_match", "am_magnum", "am_gib" }, order = 6 },
 	[2] = { offset = { 0, 0 }, atts = { "ins2_br_heavy", "ins2_br_light"  }, order = 3 },
 	[7] = { atts = { "ins2_si_kobra", "ins2_si_eotech", "ins2_si_rds", "ins2_si_2xrds", "ins2_si_c79", "ins2_si_po4x" }, order = 1 },
 	[8] = { atts = { "ins2_fg_grip" }, order = 4, sel = 1 },
@@ -349,7 +349,7 @@ SWEP.Attachments = {
 SWEP.AttachmentDependencies = {}
 SWEP.AttachmentExclusions = {
 	["ins2_br_supp"] = { "ins2_fg_grip" },
-	["ins2_fg_grip"] = { "ins2_br_supp", "ins2_br_heavy", "r6s_muzzle_2", "r6s_flashhider_2" },
+	["ins2_fg_grip"] = { "ins2_br_supp", "ins2_br_heavy", "ins2_br_light" },
 }
 
 SWEP.MuzzleAttachmentSilenced = 2
